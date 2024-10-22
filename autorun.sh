540cc8e4df42febd74045bb846a6dec1e391feb5
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-10-21 18:40:55 -0700

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+YOUR_SCRIPT= "{YOURSCRIPT.sh}" #Replace {YOURSCRIPT.sh} remove brackets
+SERVICE_NAME="{YOUR_SVC_NAME}" # Replace {YOUR_SVC_NAME} with the actual service name
+
 SCRIPT_DIR="$(dirname "$0")"
-SCRIPT_PATH="$SCRIPT_DIR/{YOURSCRIPT.sh}" #Replace {YOURSCRIPT.sh} remove brackets. 
+SCRIPT_PATH="$SCRIPT_DIR/$YOUR_SCRIPT" . 
 
 if [ ! -f "$SCRIPT_PATH" ]; then
     echo "Error: $SCRIPT_PATH does not exist."
     exit 1
 fi
 
-SERVICE_FILE="/etc/systemd/system/{YOUR_SVC_NAME}.service" #Replace {YOUR_SVC_NAME} with desired service name. 
-#Must not conflict with amy existing service names.
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service" 
 echo "[Unit]
 Description=null
 [Service]
@@ -20,5 +22,5 @@ User=root
 WantedBy=multi-user.target" | sudo tee $SERVICE_FILE > /dev/null
 
 sudo systemctl daemon-reload
-sudo systemctl enable {YOUR_SVC_NAME}.service #Replace {YOUR_SVC_NAME} with same name used in last one. 
+sudo systemctl enable $SERVICE_NAME.service
 echo "Success. The script will run at startup with superuser permissions."
