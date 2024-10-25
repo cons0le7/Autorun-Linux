d15bfab90a32e4d364984e6f38b8c452604a3174
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-10-24 19:01:01 -0700

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-#Replace {YOURSCRIPT.sh} remove brackets.
-YOUR_SCRIPT= "{YOURSCRIPT.sh}" 
-# Replace {YOUR_SVC_NAME} with the service name youd like to use. 
-#Must not conflict with any existing service names. 
-SERVICE_NAME="{YOUR_SVC_NAME}" 
+# Replace YOUR_SCRIPT.py with the name of your Python script.
+YOUR_SCRIPT="YOUR_SCRIPT.py"
+
+# Replace YOUR_SVC_NAME with the service name you'd like to use.
+# Must not conflict with any existing service names.
+SERVICE_NAME="YOUR_SVC_NAME"
 
 SCRIPT_DIR="$(dirname "$0")"
-SCRIPT_PATH="$SCRIPT_DIR/$YOUR_SCRIPT" . 
+SCRIPT_PATH="$SCRIPT_DIR/$YOUR_SCRIPT"
 
 if [ ! -f "$SCRIPT_PATH" ]; then
     echo "Error: $SCRIPT_PATH does not exist."
     exit 1
 fi
 
-SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service" 
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
+
 echo "[Unit]
 Description=null
 [Service]
 Type=simple
-ExecStart=/bin/bash $SCRIPT_PATH
+ExecStart=/usr/bin/python3 $SCRIPT_PATH
 User=root
 [Install]
 WantedBy=multi-user.target" | sudo tee $SERVICE_FILE > /dev/null
 
 sudo systemctl daemon-reload
 sudo systemctl enable $SERVICE_NAME.service
+
 echo "Success. The script will run at startup with superuser permissions."
